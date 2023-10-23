2ce0526bf9864ef5eb973ab658670bbe62eb6368
Murat Dzhulkuttiev <dzhulk@gmail.com>
2023-10-24 01:57:35 +0300

    Use proper syntax for line_metadata relation

--- spec/support/line_metadata.rb
+++ spec/support/line_metadata.rb
@@ -1,2 +1,18 @@
 require 'double_entry/line_metadata'
 require 'double_entry/line'
+
+class DoubleEntry::Line
+  def metadata
+    if DoubleEntry.config.json_metadata
+      read_attribute(:metadata)
+    end
+  end
+
+  def metadata=(meta)
+    if DoubleEntry.config.json_metadata
+      write_attribute(:metadata, meta)
+    else
+      raise "Can't write to metadata"
+    end
+  end
+end
